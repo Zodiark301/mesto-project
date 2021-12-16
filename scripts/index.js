c956c98a93eb7d88f7818cdfe54c879af356e8c7
Zodiark <admizodiark@gmail.com>
2021-12-16 22:31:33 +0300

    Добвлены 6 стандартных карточек

--- scripts/index.js
+++ scripts/index.js
@@ -9,17 +9,36 @@ const profileJob = profileInfo.querySelector('.profile__description');
 const nameInput = document.querySelector('[name="name"]');
 const jobInput = document.querySelector('[name="about"]');
 
+// Кнопки добавление карточек
+//const addCardPopup = document.querySelector('.popup_card');
+//const addButtonProfile = container.querySelector('.profile__info');
+const buttonAddCard = document.querySelector('.profile__button_type_add');
+const formElementadd = document.querySelector('[name="addButton"]');
+//const closeButton = addCardPopup.querySelector('.popup__close-button');
+
+
+// Открытие попап окна
 function openPopUp() {
   popUp.classList.add('popup_opened');
 }
 
+// Закрытие попап окна
 function closePopUp() {
   popUp.classList.remove('popup_opened');
 }
 
+// Обработчик события Click на открытие попапа
 editButton.addEventListener('click', openPopUp);
 closeButton.addEventListener('click', closePopUp);
 
+// Обработчик события Click
+function openAdd() {
+  formElementadd.addEventListener('click', openPopUp);
+  closeButton.addEventListener('click', closePopUp);
+}
+openAdd();
+
+// Получение данных из полей
 editButton.addEventListener('click', function () {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileJob.textContent;
@@ -37,6 +56,7 @@ function formSubmitHandler(evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
+// Массив стандартных карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -64,17 +84,36 @@ const initialCards = [
   }
 ];
 
+// Поиск элементов одной карточки
 const elementCard = document.querySelector('#cards').content;
-const containerElements = document.querySelector('.elements');
+const containerElements = document.querySelector('.elements__list');
 
+// template шаблон одной карточки
 function addCards(imageUrl, descriptionName) {
+  // Клонирование template шаблона
   const elementDiv = elementCard.querySelector('.elements__card').cloneNode(true);
-
+  // Ищем класс кнопки сердечка карточки
+  const cardLike = elementDiv.querySelector('.elements__like');
+  // Поля template шаблона
   elementDiv.querySelector('.elements__image').src = imageUrl;
   elementDiv.querySelector('.elements__image').alt = descriptionName;
   elementDiv.querySelector('.elements__header').textContent = descriptionName;
+  // Вызов функции лайков каждой карточки
+  cardLike.addEventListener('click', pressLike);
   return elementDiv;
-
 }
 
-containerElements.append(initialCards[1], initialCards[2]);
+// Ищем и меняем класс в лайках
+function pressLike(event) {
+  event.target.classList.toggle('elements__like_active');
+};
+
+// Добавляем стандартные карточки
+const standartCards = initialCards.map(function (elementDiv) {
+  // Возвращаем элементы объекта из массива
+  return addCards(elementDiv.link, elementDiv.name);
+});
+// Добавляем карточки в ко
+containerElements.prepend(...standartCards);
+
+
