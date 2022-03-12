d8c2004dd708cb2f52cd6aca536aade77b2c53f8
Zodiark301 <admizodiark@gmail.com>
2022-03-12 19:08:16 +0300

    feat: добавить валидацию popup окон

--- src/index.js
+++ src/index.js
@@ -1,4 +1,113 @@
 import './pages/index.css';
+
+const buttonEscKey = 27;
+
+const handleEscUp = (evt) => {
+  if (evt.keyCode === buttonEscKey) {
+    const activePopup = document.querySelector('.popup_opened');
+    closePopup(activePopup);
+  }
+};
+
+const handleClickOverlay = (evt) => {
+  if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+    const activePopup = document.querySelector('.popup_opened');
+    closePopup(activePopup);
+  }
+};
+
+//----------------Валидация---------------------//
+
+const validationConfig = {
+  formSelector: '.popup__form', // Ищем по классу все формы
+  inputSelector: '.popup__input', // Ищем все поля по классу
+  inputInvalidClass: 'popup__input_invalid',
+  errorClass: 'popup__error_visible',
+  buttonSelector: '.popup__button',
+  buttonDisabledClass: 'popup__button_disabled',
+};
+
+const showInputError = (inputElement, errorElement, errorMessage, config) => {
+  inputElement.classList.add(config.inputInvalidClass);
+  errorElement.classList.add(config.errorClass);
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (inputElement, errorElement, config) => {
+  inputElement.classList.remove(config.inputInvalidClass);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+};
+
+const checkinputValidity = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
+
+  if (inputElement.validity.valid) {
+    hideInputError(inputElement, errorElement, config)
+  } else {
+    showInputError(inputElement, errorElement, inputElement.validationMessage, config);
+  }
+};
+
+// Функция добавления класса кнопки и выключения кнопки для нажатия
+const disableButton = (buttonElement, config) => {
+  buttonElement.classList.add(config.buttonDisabledClass);
+  buttonElement.disabled = true;
+};
+
+// Функция удаления класса кнопки включения кнопки для нажатия
+const enableButton = (buttonElement, config) => {
+  buttonElement.classList.remove(config.buttonDisabledClass);
+  buttonElement.disabled = false;
+};
+
+// Проверка всех полей формы на правильность заполнения
+const hasInvalidInput = (inputList) => {
+  return inputList.some(inputElement => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (formElement, inputList, config) => {
+  const buttonElement = formElement.querySelector(config.buttonSelector);
+
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonElement, config);
+  } else {
+    enableButton(buttonElement, config);
+  }
+};
+
+const setEventListener = (formElement, config) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', () => {
+      checkinputValidity(formElement, inputElement, config);
+      toggleButtonState(formElement, inputList, config);
+      console.log('here2');
+    });
+  });
+
+  toggleButtonState(formElement, inputList, config);
+};
+// Ищем все формы по классу
+const enableValidation = (config) => { // Делаем функцию принимающую найденные поля в форме из объекта validationConfig
+  const forms = Array.from(document.querySelectorAll(config.formSelector)) // Помещаем все в переменную и ищем все формы в документе по классу
+  // помощью Array.from делаем из псевдомассива обычный массив для дальнейшего удобного использования
+
+  forms.forEach(formElement => { // Проходимся по массиву полей и отменяем для них стандартное поведение отправки формы
+    formElement.addEventListener('submit', event => { // Добавляем слушатель на кнопку отправки
+      event.preventDefault(); // Отменяем стандартное поведение кнопки отправки
+    });
+    setEventListener(formElement, config);
+  });
+};
+
+enableValidation(validationConfig); // Вызываем функцию отменяющую стандартное поведение кнопки отправки и передаем ей все найденные поля из объекта validationConfig
+
+//----------------Валидация---------------------//
+
 // Массив стандартных карточек
 const initialCards = [
   {
@@ -56,10 +165,14 @@ const placesList = document.querySelector('.elements__list');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', handleEscUp);
+  popup.addEventListener('click', handleClickOverlay);
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleEscUp);
+  popup.removeEventListener('click', handleClickOverlay);
 }
 
 editProfileButton.addEventListener('click', function () {
