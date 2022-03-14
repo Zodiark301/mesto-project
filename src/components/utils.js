bd3edfbea1b8d70e1db5d0ef4a483f8008c4b612
Zodiark301 <admizodiark@gmail.com>
2022-03-14 11:48:35 +0300

    feat: разделить все файлы по модулям

---
+++ src/components/utils.js
@@ -0,0 +1,36 @@
+import { newCardNameInput, newCardLinkInput } from './modal.js';
+
+const buttonEscKey = 27;
+
+const handleEscUp = (evt) => {
+  if (evt.keyCode === buttonEscKey) {
+    const activePopup = document.querySelector('.popup_opened');
+    closePopup(activePopup);
+  }
+};
+
+const handleClickOverlay = (evt) => {
+  if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+    const activePopup = document.querySelector('.popup_opened');
+    closePopup(activePopup);
+  }
+};
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  document.addEventListener('keydown', handleEscUp);
+  popup.addEventListener('click', handleClickOverlay);
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleEscUp);
+  popup.removeEventListener('click', handleClickOverlay);
+}
+
+function clearCardForm() {
+  newCardNameInput.value = "";
+  newCardLinkInput.value = "";
+}
+
+export { openPopup, closePopup, clearCardForm };
