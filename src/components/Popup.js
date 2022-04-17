91682c7d8d59079aaaf18cf5c8998bf0ad7fcdc4
Zodiark301 <admizodiark@gmail.com>
2022-04-17 15:39:36 +0300

    Esc

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,31 +3,34 @@ export default class Popup {
     this._popupElement = document.querySelector(popupSelector);
     this._closeButton = this._popupElement.querySelector('.popup__close');
   }
+
   open() {
     this._popupElement.classList.add('popup_opened');
+    document.addEventListener('keydown', this.escClose);
+    this._popupElement.addEventListener('mousedown', this.overlayClose);
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this.escClose);
+    this._popupElement.removeEventListener('mousedown', this.overlayClose);
   }
 
-  escClose(evt) {
-    if (evt.key === 'Escape') this.close();
-  }
+  escClose = (evt) => {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  };
 
-  overlayClose(evt) {
-    if (evt.target.classList.contains('popup')) this.close();
-  }
+  overlayClose = (evt) => {
+    if (evt.target.classList.contains('popup')) {
+      this.close();
+    }
+  };
 
   setEventListeners() {
     this._closeButton.addEventListener(('click'), () => {
       this.close();
-    })
-    document.addEventListener('keydown', (evt) => {
-      this.escClose(evt);
-    });
-    this._popupElement.addEventListener('click', (evt) => {
-      this.overlayClose(evt);
     })
   }
 }
